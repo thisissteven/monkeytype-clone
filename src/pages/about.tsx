2e5c4e145a88c6899b9ebbbf4c6f3be2e9fa832d
Steven <ssteven075@gmail.com>
2022-07-19 15:57:33 +0700

    feat: add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,27 @@
 // !STARTERCONF You can delete this page
 import clsx from 'clsx';
 import * as React from 'react';
+import {
+  SiGraphql,
+  SiNextdotjs,
+  SiReact,
+  SiStrapi,
+  SiTailwindcss,
+} from 'react-icons/si';
 
 import AnimateFade from '@/components/layout/AnimateFade';
 import ArrowLink from '@/components/links/ArrowLink';
+import UnderlineLink from '@/components/links/UnderlineLink';
 import Seo from '@/components/Seo';
+import Tooltip from '@/components/Tooltip';
+
+const techs = [
+  { icon: <SiNextdotjs />, key: 'next' },
+  { icon: <SiReact />, key: 'react' },
+  { icon: <SiGraphql />, key: 'graphql' },
+  { icon: <SiTailwindcss />, key: 'tailwind' },
+  { icon: <SiStrapi />, key: 'strapi' },
+];
 
 export default function AboutPage() {
   return (
@@ -13,11 +30,36 @@ export default function AboutPage() {
 
       <main>
         <section>
-          <div className={clsx('layout min-h-screen py-10')}>
-            <ArrowLink direction='left' className='my-4 text-font' href='/'>
-              back to home
-            </ArrowLink>
+          <div className={clsx('layout flex min-h-[65vh] flex-col pt-10')}>
+            <div>
+              <ArrowLink direction='left' className='my-4 text-font' href='/'>
+                back to home
+              </ArrowLink>
+            </div>
             <h1>about</h1>
+            <div className='mt-4'>
+              <p>
+                this app is a clone of{' '}
+                <UnderlineLink
+                  className='text-fg'
+                  href='https://monkeytype.com'
+                >
+                  monkeytype
+                </UnderlineLink>
+              </p>
+              <p className='my-4'>technologies used:</p>
+              <div className='flex gap-4 text-4xl text-hl'>
+                {techs.map((tech) => (
+                  <div className='group relative' key={tech.key}>
+                    {tech.icon}
+                    <Tooltip className='top-12 group-hover:translate-y-0 group-hover:opacity-100'>
+                      {tech.key}
+                    </Tooltip>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <p className='flex flex-1 items-end'>- 12th of July, 2022</p>
           </div>
         </section>
       </main>
