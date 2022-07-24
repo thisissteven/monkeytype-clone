ba31f19cc2c292c1b13a199cd86a9e1527c5bd98
Steven <ssteven075@gmail.com>
2022-07-24 23:36:13 +0700

    feat: add leaderboard skeleton, remove filters

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,21 +1,17 @@
 // !STARTERCONF You can delete this page
 import { gql, useQuery } from '@apollo/client';
 import clsx from 'clsx';
-import { AnimatePresence, motion } from 'framer-motion';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
 import Link from 'next/link';
 import * as React from 'react';
 import { FaCrown, FaUserCircle } from 'react-icons/fa';
-import { VscDebugRestart } from 'react-icons/vsc';
-import { useInView } from 'react-intersection-observer';
 
 import clsxm from '@/lib/clsxm';
 
 import AnimateFade from '@/components/layout/AnimateFade';
 import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
-import Tooltip from '@/components/Tooltip';
 
 import { useAuthState } from '@/context/User/UserContext';
 
@@ -40,16 +36,10 @@ type Leaderboard = {
 };
 
 const GetLeaderboards = gql`
-  query GetLeaderboards(
-    $page: Int!
-    $pageSize: Int!
-    $type: String
-    $time: Int
-  ) {
+  query GetLeaderboards($page: Int!, $pageSize: Int!) {
     leaderboards(
       sort: "wpm:desc"
       pagination: { page: $page, pageSize: $pageSize }
-      filters: { type: { eq: $type }, and: { time: { eq: $time } } }
     ) {
       data {
         id
@@ -76,12 +66,8 @@ const GetLeaderboards = gql`
   }
 `;
 
-const filterByType = ['words', 'sentences', 'numbers'];
-const filterByTime = [15, 30, 45, 60, 120];
-
 export default function LeaderboardPage() {
-  const { ref, inView } = useInView();
-  const { data, refetch, fetchMore } = useQuery(GetLeaderboards, {
+  const { data } = useQuery(GetLeaderboards, {
     variables: { page: 1, pageSize: 100 },
     pollInterval: 500,
   });
@@ -92,79 +78,6 @@ export default function LeaderboardPage() {
 
   // Create formatter (English).
   const timeAgo = new TimeAgo('en-US');
-
-  const [page, setPage] = React.useState(1);
-  const [pageCount, setPageCount] = React.useState(() => 0);
-
-  const [type, setType] = React.useState('');
-  const [time, setTime] = React.useState(0);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const filteredData = React.useMemo(
-    () =>
-      data?.leaderboards?.data.filter((item: Leaderboard) => {
-        if (time && !type) {
-          return item.attributes.time === time;
-        } else if (type && !time) {
-          return item.attributes.type === type;
-        } else if (time && type) {
-          return item.attributes.time === time && item.attributes.type === type;
-        } else {
-          return data?.leaderboards?.data;
-        }
-      }),
-    [time, type, data]
-  );
-
-  React.useEffect(() => {
-    if (page > pageCount) return;
-    fetchMore({
-      variables: { page: page, pageSize: 100 },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (!fetchMoreResult) {
-          return prev;
-        }
-
-        const currentLeaderboards: { leaderboards: { data: Leaderboard[] } } = {
-          leaderboards: {
-            ...prev.leaderboards,
-            data: [
-              ...prev.leaderboards.data,
-              ...fetchMoreResult.leaderboards.data,
-            ],
-          },
-        };
-
-        return currentLeaderboards;
-      },
-    }).then(() => setPage((page) => page + 1));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
-
-  React.useEffect(() => {
-    if (data) {
-      const {
-        leaderboards: {
-          meta: {
-            pagination: { pageCount },
-          },
-        },
-      } = data;
-      setPageCount(pageCount);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
-
-  React.useEffect(() => {
-    if (page >= pageCount) return;
-    if (inView) {
-      setPage((page) => {
-        if (page >= pageCount) return page;
-        return page + 1;
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inView]);
 
   return (
     <AnimateFade>
@@ -187,79 +100,7 @@ export default function LeaderboardPage() {
                 </Link>
               )}
             </div>
-            <div className='mb-2 flex items-center justify-between'>
-              <h1 className='text-hl'>leaderboard</h1>
-              <button
-                onClick={() =>
-                  refetch().then(() => {
-                    setPage(1);
-                    setType('');
-                    setTime(0);
-                  })
-                }
-                className='relative mt-2 flex items-center rounded-lg border-0 px-4 py-2 outline-none '
-              >
-                <VscDebugRestart className='peer scale-x-[-1] transform text-2xl text-fg/50  transition-colors duration-200 hover:text-fg' />
-                <Tooltip
-                  className='top-12 bg-fg font-primary peer-hover:translate-y-0 peer-hover:opacity-100'
-                  triangle='bg-fg'
-                >
-                  Refresh data
-                </Tooltip>
-              </button>
-            </div>
-
-            <div className='my-4 flex flex-wrap items-center justify-end gap-4 font-primary sm:justify-between'>
-              <div className='hidden sm:block'>
-                <p>filter by:</p>
-              </div>
-              <div className='flex flex-wrap justify-end gap-2'>
-                <div className='flex gap-2'>
-                  <button
-                    className={clsxm(
-                      [!type && !time ? 'text-fg' : 'text-fg/50'],
-                      'transition-colors duration-200 hover:text-fg'
-                    )}
-                    onClick={() => {
-                      setType('');
-                      setTime(0);
-                    }}
-                  >
-                    all
-                  </button>
-                  {filterByType.map((item) => {
-                    return (
-                      <button
-                        key={item}
-                        className={clsxm(
-                          [type === item ? 'text-fg' : 'text-fg/50'],
-                          'transition-colors duration-200 hover:text-fg'
-                        )}
-                        onClick={() => setType(item)}
-                      >
-                        {item}
-                      </button>
-                    );
-                  })}
-                </div>
-                <div className='flex gap-2'>
-                  {filterByTime.map((item) => {
-                    return (
-                      <button
-                        key={item}
-                        className={clsxm(
-                          [time === item ? 'text-fg' : 'text-fg/50'],
-                          'transition-colors duration-200 hover:text-fg'
-                        )}
-                        onClick={() => setTime(item)}
-                      >
-                        {item}
-                      </button>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
+            <h1 className='mb-4 text-hl'>top 100 leaderboard</h1>
 
             <div className='relative overflow-auto rounded-lg bg-hl/50 p-2'>
               <table className='w-full overflow-hidden rounded-lg font-primary'>
@@ -274,82 +115,104 @@ export default function LeaderboardPage() {
                   </tr>
                 </thead>
 
-                <AnimatePresence exitBeforeEnter>
-                  <motion.tbody
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    layout
-                    key={time + type}
-                  >
-                    {filteredData?.map(
-                      (leaderboard: Leaderboard, index: number) => {
-                        const {
-                          attributes: {
-                            wpm,
-                            time,
-                            type,
-                            createdAt,
-                            user: {
-                              data: {
-                                attributes: { username },
-                              },
-                            },
+                {!data?.leaderboards?.data && (
+                  <>
+                    <tr className='h-14 w-full animate-pulse border-t-4 border-hl'>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                    </tr>
+                    <tr className='h-14 w-full animate-pulse border-t-4 border-hl'>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                    </tr>
+                    <tr className='h-14 w-full animate-pulse border-t-4 border-hl'>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                      <td className='bg-fg'></td>
+                    </tr>
+                    <tr className='h-14 w-full animate-pulse border-t-4 border-hl'>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                      <td className='bg-fg/80'></td>
+                    </tr>
+                  </>
+                )}
+                {data?.leaderboards?.data?.map(
+                  (leaderboard: Leaderboard, index: number) => {
+                    const {
+                      attributes: {
+                        wpm,
+                        time,
+                        type,
+                        createdAt,
+                        user: {
+                          data: {
+                            attributes: { username },
                           },
-                        } = leaderboard;
-                        return (
-                          <tr
-                            key={index}
+                        },
+                      },
+                    } = leaderboard;
+                    return (
+                      <tr
+                        key={index}
+                        className={clsxm(
+                          'whitespace-nowrap border-t-4 border-hl',
+                          [index % 2 === 0 ? 'bg-fg' : 'bg-fg/80']
+                        )}
+                      >
+                        <td className='py-3 pl-4'>
+                          <span className='text-bg'>
+                            {/* first rank */}
+                            {index === 0 ? (
+                              <FaCrown className='my-1' />
+                            ) : (
+                              index + 1
+                            )}
+                          </span>
+                        </td>
+
+                        <td className='px-2 md:px-0'>
+                          <div className='flex items-center gap-2 text-bg'>
+                            <FaUserCircle /> {username}
+                          </div>
+                        </td>
+
+                        <td className='px-2 text-bg md:px-0'>
+                          <span
                             className={clsxm(
-                              'whitespace-nowrap border-t-4 border-hl',
-                              [index % 2 === 0 ? 'bg-fg' : 'bg-fg/80']
+                              'rounded-md bg-bg px-2 py-1 text-xs text-fg'
                             )}
                           >
-                            <td className='py-3 pl-4'>
-                              <span className='text-bg'>
-                                {/* first rank */}
-                                {index === 0 ? (
-                                  <FaCrown className='my-1' />
-                                ) : (
-                                  index + 1
-                                )}
-                              </span>
-                            </td>
-
-                            <td className='px-2 md:px-0'>
-                              <div className='flex items-center gap-2 text-bg'>
-                                <FaUserCircle /> {username}
-                              </div>
-                            </td>
-
-                            <td className='px-2 text-bg md:px-0'>
-                              <span
-                                className={clsxm(
-                                  'rounded-md bg-bg px-2 py-1 text-xs text-fg'
-                                )}
-                              >
-                                {wpm} wpm
-                              </span>
-                            </td>
-                            <td className='px-2 text-sm text-bg md:px-0'>
-                              {type}
-                            </td>
-                            <td className='px-2 text-sm text-bg md:px-0'>
-                              {time}s
-                            </td>
-                            <td className='px-2 text-sm text-bg md:px-0'>
-                              {timeAgo.format(new Date(createdAt))}
-                            </td>
-                          </tr>
-                        );
-                      }
-                    )}
-                  </motion.tbody>
-                </AnimatePresence>
+                            {wpm} wpm
+                          </span>
+                        </td>
+                        <td className='px-2 text-sm text-bg md:px-0'>{type}</td>
+                        <td className='px-2 text-sm text-bg md:px-0'>
+                          {time}s
+                        </td>
+                        <td className='px-2 text-sm text-bg md:px-0'>
+                          {timeAgo.format(new Date(createdAt))}
+                        </td>
+                      </tr>
+                    );
+                  }
+                )}
               </table>
             </div>
-
-            <div ref={ref}></div>
           </div>
         </section>
       </main>
