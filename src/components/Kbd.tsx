96556e08adc49938ee5505f88e034acbfaa255b3
Steven <70851099+steven2801@users.noreply.github.com>
2022-07-25 17:46:53 +0700

    Delete src/components directory

--- src/components/Kbd.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default function Kbd({ children }: { children: React.ReactNode }) {
-  return (
-    <div className='flex items-center rounded-sm border-b-2 border-font bg-hl px-1 text-sm font-medium text-bg'>
-      {children}
-    </div>
-  );
-}
