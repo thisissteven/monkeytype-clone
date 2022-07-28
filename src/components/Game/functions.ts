cfa3658bb16cd1457c98c084e3c1229dac02cdba
Steven <ssteven075@gmail.com>
2022-07-28 14:18:51 +0700

    feat: update sentences and words list

--- src/components/Game/functions.ts
+++ src/components/Game/functions.ts
@@ -8,15 +8,15 @@ const _ = require('lodash');
 export const shuffleList = (type: string) => {
   switch (type) {
     case 'words':
-      return _.shuffle(words).slice(0, 100);
+      return _.shuffle(words).slice(0, 150);
     case 'numbers':
-      return _.shuffle(numbers).slice(0, 30);
+      return _.shuffle(numbers).slice(0, 50);
     case 'sentences':
       // eslint-disable-next-line no-case-declarations
       let sentencesArray = _.shuffle(sentences);
-      sentencesArray = sentencesArray.slice(0, 8);
+      sentencesArray = sentencesArray.slice(0, 12);
       return sentencesArray;
     default:
-      return _.shuffle(words).slice(0, 1000);
+      return _.shuffle(words).slice(0, 150);
   }
 };
